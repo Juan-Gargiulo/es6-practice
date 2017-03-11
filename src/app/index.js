14c0ffd4c0f28f5cd31beb4fb1dbb5b66f98f87b
juan <juanpab84@gmail.com>
2017-03-11 13:40:53 -0300

    primeras pruebas

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,25 @@
-console.log("funciono");
+{
+    let arr = ['a', 'b', 'c'];
+
+    //prueba array functions
+    console.log( arr.filter( (e)=> e !== 'a' ) );
+    console.log( arr.reduce( (e, n)=> e+n ) );
+
+    //agrega un elemento pero no modifica el arreglo
+    console.log( arr.concat(['d']) );
+
+    //esto hace lo mismo pero modifica el arreglo original
+    arr.splice( arr.length, 0, 'd' );
+    console.log(arr);
+
+}
+
+//destructuring
+{
+    const arrObj = { b: 1, c: { foo: 1, bar: 'hola' } }
+    const {c} = arrObj // tomo solo 'c' del array
+    console.log(c);
+}
+
+//block scopes es6, replazo de iffes
+console.log(c); //c aca esta fuera de los block scopes { } = "not defined"
